f834d76f51c4d778b5b09f9ef770f2196d49bbb3
Theo Meinhardt <theo.meinhardt@batinfo.at>
2026-03-18 15:03:51 +0100

    Feat: Basic TCP Listener
    - Implements `new` and `listen` Methods for our Server.
    - Listens on 127.0.0.1:1234 and printing incoming connections to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use http_webserver::server::Server;
+
 fn main() {
-    println!("Hello HTTP!");
+    let server = Server::new("127.0.0.1", 1234);
+    server.listen();
 }
--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,30 @@
+use std::{
+    io::{BufReader, BufWriter},
+    net::{TcpListener, TcpStream, ToSocketAddrs},
+};
 
+pub struct Server {
+    listener: TcpListener,
+}
+
+impl Server {
+    // Creates and binds a new TCP socket server on a specified address and port.
+    pub fn new(addr: &str, port: u32) -> Self {
+        let listener = TcpListener::bind(format!("{addr}:{port}"))
+            .expect("Failed to start server! Is your port already used?");
+
+        println!(
+            "Server listening on port {}",
+            listener.local_addr().unwrap()
+        );
+
+        Server { listener }
+    }
+
+    // Starts listening for incoming connections.
+    pub fn listen(self) {
+        for stream in self.listener.incoming() {
+            println!("Handeling {}", stream.unwrap().local_addr().unwrap().ip());
+        }
+    }
+}
