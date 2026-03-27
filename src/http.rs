3c112f6ac634fdcc9db01a84029de0940bcc70d6
Theo Meinhardt <theo.meinhardt@batinfo.at>
2026-03-27 13:01:53 +0100

    Feat: Added Body
    Added `Body` struct for basic body handeling.

--- src/http.rs
+++ src/http.rs
@@ -1,8 +1,10 @@
+mod body;
 mod headers;
 mod method;
 mod request;
 mod response;
 
+pub use body::Body;
 pub use headers::*;
 pub use method::Method;
 pub use request::Request;
---
+++ src/http/body.rs
@@ -0,0 +1,40 @@
+use crate::http::content_type::ContentType;
+
+pub struct Body {
+    content_type: Option<ContentType>,
+    content: String,
+}
+
+impl Body {
+    pub fn new(content_type: ContentType, content: String) -> Self {
+        Self {
+            content_type: Some(content_type),
+            content,
+        }
+    }
+
+    pub fn empty() -> Self {
+        Self {
+            content_type: None,
+            content: String::new(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        match self.content_type {
+            Some(_) => false,
+            None => true,
+        }
+    }
+
+    pub fn get_content_type(&self) -> &ContentType {
+        &self.content_type.as_ref().unwrap()
+    }
+
+    pub fn encode(&self) -> String {
+        let mut result = String::from("\r\n");
+        result.push_str(&self.content);
+
+        result
+    }
+}
